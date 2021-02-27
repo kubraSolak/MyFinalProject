ccd2aa8e6b46bf45776d943ed9d7a7aa8738e7d4
kübra solak <kubrasolak35@gmail.com>
2021-02-27 17:27:14 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid= "Ürün ismi eklenemedi";
+        internal static string ProductsListed = "Ürünler Listelendi";
+        internal static string MaintenanceTime = "Sistem Bakımda";
+    }
+}
